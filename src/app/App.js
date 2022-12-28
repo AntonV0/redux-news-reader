4347cecc0c2fd2b77139ccb237c374450f453485
Anton Vlasenko <anton.vlasenko94@gmail.com>
2022-12-28 23:28:35 +0000

    Fix imports & install MSW

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-// import './App.css';
+import './App.css';
 import React from 'react';
 import ArticlePreviews from '../features/articlePreviews/ArticlePreviews';
 import CurrentArticle from '../features/currentArticle/CurrentArticle';
